4b6cb4c5c7c59de1a567adfc37b93d47e8f8697e
Jakob Christoph Vogler <jakob@jcvogler.com>
2023-06-27 16:49:22 +0000

    fix: rm CustomRequest

--- src/server/lib/middlewares/CustomRequest.ts
+++
@@ -1,7 +0,0 @@
-import {NextApiRequest} from "next"
-
-export interface CustomRequest<body extends any, query extends any, params extends any> extends NextApiRequest {
-  body: body
-  query: query
-  params: params
-}
