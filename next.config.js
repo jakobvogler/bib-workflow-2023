8602416e5604dc8169e7aa61514b2af5c0d4a0b3
Jakob Christoph Vogler <jakob@jcvogler.com>
2023-07-29 15:47:48 +0200

    feat: update environment

--- next.config.js
+++ next.config.js
@@ -6,8 +6,14 @@ const nextConfig = {
     OWN_URL: process.env.OWN_URL,
   },
   serverRuntimeConfig: {
+    PERFORMANCE_URL: process.env.PERFORMANCE_URL,
     HASH_SECRET: process.env.HASH_SECRET,
     MONGO_URI: process.env.MONGO_URI,
+
+    GCP_PROJECT_ID: process.env.GCP_PROJECT_ID,
+    GCP_LOCATION_ID: process.env.GCP_LOCATION_ID,
+
+    CLOUD_TASKS_QUEUE_ID: process.env.CLOUD_TASKS_QUEUE_ID,
   },
 }
 
